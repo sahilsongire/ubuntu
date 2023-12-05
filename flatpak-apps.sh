9bbe33020bdbdddcc56b158899efd0b583a3d64e
Sahil Songire <142686722+sahilsongire@users.noreply.github.com>
2023-12-05 15:35:21 +0530

    Update flatpak-apps.sh

--- flatpak-apps.sh
+++ flatpak-apps.sh
@@ -1,15 +1,15 @@
 !#/bin/bash
 echo "Installing your favourite apps.........."
-flatpak install flathub org.mozilla.firefox #firefox(flatpak version)
-flatpak install flathub com.discordapp.Discord #Discord
-flatpak install flathub org.videolan.VLC #VLC
-flatpak install flathub org.telegram.desktop #telegram
-flatpak install flathub com.obsproject.Studio #OBS-Studio
-flatpak install flathub com.bitwarden.desktop #Bitwarden(password storing)
-flatpak install flathub io.github.mimbrero.WhatsAppDesktop #whatsapp desktop
-flatpak install flathub org.signal.Signal #Signal
-flatpak install flathub com.github.micahflee.torbrowser-launcher #TOR browser
-flatpak install flathub org.kde.kdenlive #kdenlive for video editing
-flatpak install flathub org.audacityteam.Audacity #audicity for audio recording
-flatpak install flathub com.google.AndroidStudio #android studio code
-flatpak install flathub com.vscodium.codium #vscodium 
+flatpak install flathub org.mozilla.firefox -y #firefox(flatpak version)
+flatpak install flathub com.discordapp.Discord -y #Discord
+flatpak install flathub org.videolan.VLC -y #VLC
+flatpak install flathub org.telegram.desktop -y #telegram
+flatpak install flathub com.obsproject.Studio -y #OBS-Studio
+flatpak install flathub com.bitwarden.desktop -y #Bitwarden(password storing)
+flatpak install flathub io.github.mimbrero.WhatsAppDesktop -y #whatsapp desktop
+flatpak install flathub org.signal.Signal -y #Signal
+flatpak install flathub com.github.micahflee.torbrowser-launcher -y #TOR browser
+flatpak install flathub org.kde.kdenlive -y #kdenlive for video editing
+flatpak install flathub org.audacityteam.Audacity -y #audicity for audio recording
+flatpak install flathub com.google.AndroidStudio -y #android studio code
+flatpak install flathub com.vscodium.codium -y #vscodium 
