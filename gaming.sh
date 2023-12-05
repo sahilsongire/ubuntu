a0384499c8578b51c5a8aef986479d2c0b9fd9d8
Sahil Songire <142686722+sahilsongire@users.noreply.github.com>
2023-12-05 15:35:49 +0530

    Update gaming.sh

--- gaming.sh
+++ gaming.sh
@@ -1,6 +1,6 @@
 !#/bin/bash
-flatpak install flathub org.ppsspp.PPSSPP #psp emulator
-flatpak install flathub net.kuribo64.melonDS #nintendo ds
-flatpak install flathub com.valvesoftware.Steam #steam
-flatpak install flathub net.lutris.Lutris #litrus(it can play retro games including NES,SNES and others)
-flatpak install flathub net.pcsx2.PCSX2 #ps2 emulator
+flatpak install flathub org.ppsspp.PPSSPP -y #psp emulator
+flatpak install flathub net.kuribo64.melonDS -y #nintendo ds
+flatpak install flathub com.valvesoftware.Steam -y #steam
+flatpak install flathub net.lutris.Lutris -y #litrus(it can play retro games including NES,SNES and others)
+flatpak install flathub net.pcsx2.PCSX2 -y #ps2 emulator
