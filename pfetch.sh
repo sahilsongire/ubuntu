c1d8d03570e6deabe183132dd033e36676910bda
Sahil Songire <142686722+sahilsongire@users.noreply.github.com>
2023-12-05 15:25:22 +0530

    Update pfetch.sh

--- pfetch.sh
+++ pfetch.sh
@@ -2,4 +2,4 @@
 echo "For Terminal Pfetch"
 wget https://github.com/dylanaraps/pfetch/archive/master.zip
 unzip master.zip
-sudo install pfetch-master/pfetch /usr/local/bin/ -y
+sudo install pfetch-master/pfetch /usr/local/bin/ 
