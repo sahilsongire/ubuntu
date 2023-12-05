92c2f841057f218b7e12a93c9b3ca5b117532f2d
Sahil Songire <142686722+sahilsongire@users.noreply.github.com>
2023-12-05 13:36:37 +0530

    Add files via upload
    Apps Required for Gaming.....
    ^w^ Have fun

---
+++ gaming.sh
@@ -0,0 +1,6 @@
+!#/bin/bash
+flatpak install flathub org.ppsspp.PPSSPP #psp emulator
+flatpak install flathub net.kuribo64.melonDS #nintendo ds
+flatpak install flathub com.valvesoftware.Steam #steam
+flatpak install flathub net.lutris.Lutris #litrus(it can play retro games including NES,SNES and others)
+flatpak install flathub net.pcsx2.PCSX2 #ps2 emulator
