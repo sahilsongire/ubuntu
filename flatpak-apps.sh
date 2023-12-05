cf978e2f369797bc351eb2a5752ef41e0f102295
Sahil Songire <142686722+sahilsongire@users.noreply.github.com>
2023-12-05 13:32:20 +0530

    Add files via upload
    Flatpak apps for daily usage

---
+++ flatpak-apps.sh
@@ -0,0 +1,15 @@
+!#/bin/bash
+echo "Installing your favourite apps.........."
+flatpak install flathub org.mozilla.firefox #firefox(flatpak version)
+flatpak install flathub com.discordapp.Discord #Discord
+flatpak install flathub org.videolan.VLC #VLC
+flatpak install flathub org.telegram.desktop #telegram
+flatpak install flathub com.obsproject.Studio #OBS-Studio
+flatpak install flathub com.bitwarden.desktop #Bitwarden(password storing)
+flatpak install flathub io.github.mimbrero.WhatsAppDesktop #whatsapp desktop
+flatpak install flathub org.signal.Signal #Signal
+flatpak install flathub com.github.micahflee.torbrowser-launcher #TOR browser
+flatpak install flathub org.kde.kdenlive #kdenlive for video editing
+flatpak install flathub org.audacityteam.Audacity #audicity for audio recording
+flatpak install flathub com.google.AndroidStudio #android studio code
+flatpak install flathub com.vscodium.codium #vscodium 
