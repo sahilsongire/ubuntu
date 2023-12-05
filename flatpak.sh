b14fd29c887182ad4d24b1b3e8381e1d0577bdba
Sahil Songire <142686722+sahilsongire@users.noreply.github.com>
2023-12-05 13:21:05 +0530

    Create flatpak.sh
    It's a script for Flatpak Repo install it and restart your system and enjoy the flatpak apps

---
+++ flatpak.sh
@@ -0,0 +1,5 @@
+!#/bin/bash
+echo "Installing Flatpak repo"
+sudo apt install flatpak -y
+sudo apt install gnome-software-plugin-flatpak -y
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo    
