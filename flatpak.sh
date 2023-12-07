8b3388f79c54940c8972f139124bc3e1d286b3c6
Sahil Songire <142686722+sahilsongire@users.noreply.github.com>
2023-12-07 11:33:43 +0530

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -3,5 +3,5 @@ echo "Installing Flatpak repo"
 sudo apt install flatpak -y
 sudo apt install gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo    
-echo ">>>> Rebooting the system>>>>"
+echo "<<<<Rebooting the system........."
 reboot
