ad0c3d3a56ff29230b4215d94fb6720d7daa5c32
Sahil Songire <142686722+sahilsongire@users.noreply.github.com>
2023-12-07 16:34:17 +0530

    Update chat.sh

--- chat.sh
+++ chat.sh
@@ -5,3 +5,5 @@ echo "Installing Telegram........"
 flatpak install flathub org.telegram.desktop -y #telegram
 echo "Installing Signal........"
 flatpak install flathub org.signal.Signal -y #Signal
+echo "Installing Whatsapp........"
+flatpak install flathub io.github.mimbrero.WhatsAppDesktop -y #whatsapp desktop
