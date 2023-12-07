2cab61f74c65ea56ca9914445ddbdc79d531ef75
Sahil Songire <142686722+sahilsongire@users.noreply.github.com>
2023-12-07 16:35:30 +0530

    General apps for daily use

---
+++ general.sh
@@ -0,0 +1,18 @@
+!#/bin/bash
+echo "Installing your favourite apps.........."
+echo "Installing firefox........"
+flatpak install flathub org.mozilla.firefox -y #firefox(flatpak version)
+echo "Installing VLC........"
+flatpak install flathub org.videolan.VLC -y #VLC
+echo "Installing OBS-STUDIO........"
+flatpak install flathub com.obsproject.Studio -y #OBS-Studio
+echo "Installing Bitwarden........"
+flatpak install flathub com.bitwarden.desktop -y #Bitwarden(password storing)
+echo "Installing Tor-Browser........"
+flatpak install flathub com.github.micahflee.torbrowser-launcher -y #TOR browser
+echo "Installing Kdenlive........"
+flatpak install flathub org.kde.kdenlive -y #kdenlive for video editing
+echo "Installing Audacity........"
+flatpak install flathub org.audacityteam.Audacity -y #audicity for audio recording
+echo "Installing VSCODIUM........"
+flatpak install flathub com.vscodium.codium -y #vscodium 
